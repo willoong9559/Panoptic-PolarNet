eed86c4481591793f4220f3d4ee31ccbe5f1d8bc
willoong <willoongl@gmail.com>
2025-07-08 16:49:40 +0800

    数据加载异步处理

--- train.py
+++ train.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+#ignore weird np warning
+import warnings
+warnings.filterwarnings("ignore")
 import os
 import argparse
 import sys
@@ -16,9 +19,9 @@ from network.instance_post_processing import get_panoptic_segmentation
 from network.loss import panoptic_loss
 from utils.eval_pq import PanopticEval
 from utils.configs import merge_configs
-#ignore weird np warning
-import warnings
-warnings.filterwarnings("ignore")
+from utils.log import log_writer
+
+log_writer = log_writer()
 
 def SemKITTI2train(label):
     if isinstance(label, list):
@@ -66,13 +69,14 @@ def main(args):
         my_model = load_pretrained_model(my_model,torch.load(model_save_path))
     elif os.path.exists(pretrained_model):
         my_model = load_pretrained_model(my_model,torch.load(pretrained_model))
-    my_model.to(pytorch_device)
+    my_model.to(pytorch_device, non_blocking=True)
 
     optimizer = optim.Adam(my_model.parameters())
     loss_fn = panoptic_loss(center_loss_weight = args['model']['center_loss_weight'], offset_loss_weight = args['model']['offset_loss_weight'],\
                             center_loss = args['model']['center_loss'], offset_loss=args['model']['offset_loss'])
 
     #prepare dataset
+    log_writer.print_write_log("dataset_path = " + data_path + '/sequences/')
     train_pt_dataset = SemKITTI(data_path + '/sequences/', imageset = 'train', return_ref = True, instance_pkl_path=args['dataset']['instance_pkl_path'])
     val_pt_dataset = SemKITTI(data_path + '/sequences/', imageset = 'val', return_ref = True, instance_pkl_path=args['dataset']['instance_pkl_path'])
     if args['model']['polar']:
@@ -81,16 +85,27 @@ def main(args):
     else:
         train_dataset=voxel_dataset(train_pt_dataset, args['dataset'], grid_size = grid_size, ignore_label = 0,use_aug = True)
         val_dataset=voxel_dataset(val_pt_dataset, args['dataset'], grid_size = grid_size, ignore_label = 0)
+    log_writer.print_write_log("Train dataset size:" + len(train_dataset))
     train_dataset_loader = torch.utils.data.DataLoader(dataset = train_dataset,
                                                     batch_size = train_batch_size,
+                                                    pin_memory=True,
                                                     collate_fn = collate_fn_BEV,
                                                     shuffle = True,
-                                                    num_workers = 4)
+                                                    persistent_workers=True,  # 保持工作进程存活
+                                                    prefetch_factor=4,
+                                                    num_workers = 8)
     val_dataset_loader = torch.utils.data.DataLoader(dataset = val_dataset,
                                                     batch_size = val_batch_size,
+                                                    pin_memory=True,
                                                     collate_fn = collate_fn_BEV,
+                                                    persistent_workers=True,  # 保持工作进程存活
+                                                    prefetch_factor=4,
                                                     shuffle = False,
-                                                    num_workers = 4)
+                                                    num_workers = 8)
+    
+    # 额外的内存优化设置
+    torch.backends.cudnn.benchmark = True  # 优化cudnn性能
+    torch.backends.cudnn.enabled = True
 
     # training
     epoch=0
@@ -110,13 +125,13 @@ def main(args):
                 evaluator.reset()
                 with torch.no_grad():
                     for i_iter_val,(val_vox_fea,val_vox_label,val_gt_center,val_gt_offset,val_grid,val_pt_labels,val_pt_ints,val_pt_fea) in enumerate(val_dataset_loader):
-                        val_vox_fea_ten = val_vox_fea.to(pytorch_device)
+                        val_vox_fea_ten = val_vox_fea.to(pytorch_device, non_blocking=True)
                         val_vox_label = SemKITTI2train(val_vox_label)
-                        val_pt_fea_ten = [torch.from_numpy(i).type(torch.FloatTensor).to(pytorch_device) for i in val_pt_fea]
-                        val_grid_ten = [torch.from_numpy(i[:,:2]).to(pytorch_device) for i in val_grid]
-                        val_label_tensor=val_vox_label.type(torch.LongTensor).to(pytorch_device)
-                        val_gt_center_tensor = val_gt_center.to(pytorch_device)
-                        val_gt_offset_tensor = val_gt_offset.to(pytorch_device)
+                        val_pt_fea_ten = [torch.from_numpy(i).type(torch.FloatTensor).to(pytorch_device, non_blocking=True) for i in val_pt_fea]
+                        val_grid_ten = [torch.from_numpy(i[:,:2]).to(pytorch_device, non_blocking=True) for i in val_grid]
+                        val_label_tensor=val_vox_label.type(torch.LongTensor).to(pytorch_device, non_blocking=True)
+                        val_gt_center_tensor = val_gt_center.to(pytorch_device, non_blocking=True)
+                        val_gt_offset_tensor = val_gt_offset.to(pytorch_device, non_blocking=True)
 
                         if visibility:
                             predict_labels,center,offset = my_model(val_pt_fea_ten, val_grid_ten, val_vox_fea_ten)
@@ -125,10 +140,10 @@ def main(args):
 
                         for count,i_val_grid in enumerate(val_grid):
                             # get foreground_mask
-                            for_mask = torch.zeros(1,grid_size[0],grid_size[1],grid_size[2],dtype=torch.bool).to(pytorch_device)
+                            for_mask = torch.zeros(1,grid_size[0],grid_size[1],grid_size[2],dtype=torch.bool).to(pytorch_device, non_blocking=True)
                             for_mask[0,val_grid[count][:,0],val_grid[count][:,1],val_grid[count][:,2]] = True
                             # post processing
-                            panoptic_labels,center_points = get_panoptic_segmentation(torch.unsqueeze(predict_labels[count], 0),torch.unsqueeze(center[count], 0),torch.unsqueeze(offset[count], 0),\
+                            panoptic_labels,center_points = get_panoptic_segmentation(torch.unsqueeze(predict_labels[count], 0),torch.unsqueeze(center[count], 0),\
                                                                                       val_pt_dataset.thing_list, threshold=args['model']['post_proc']['threshold'], nms_kernel=args['model']['post_proc']['nms_kernel'],\
                                                                                       top_k=args['model']['post_proc']['top_k'], polar=circular_padding,foreground_mask=for_mask)
                             panoptic_labels = panoptic_labels.cpu().detach().numpy().astype(np.int32)
@@ -138,36 +153,37 @@ def main(args):
                 my_model.train()
                 class_PQ, class_SQ, class_RQ, class_all_PQ, class_all_SQ, class_all_RQ = evaluator.getPQ()
                 miou,ious = evaluator.getSemIoU()
-                print('Validation per class PQ, SQ, RQ and IoU: ')
+                log_writer.print_write_log('Validation per class PQ, SQ, RQ and IoU: ')
+                
                 for class_name, class_pq, class_sq, class_rq, class_iou in zip(unique_label_str,class_all_PQ[1:],class_all_SQ[1:],class_all_RQ[1:],ious[1:]):
-                    print('%15s : %6.2f%%  %6.2f%%  %6.2f%%  %6.2f%%' % (class_name, class_pq*100, class_sq*100, class_rq*100, class_iou*100))                                  
+                    log_writer.print_write_log('%15s : %6.2f%%  %6.2f%%  %6.2f%%  %6.2f%%' % (class_name, class_pq*100, class_sq*100, class_rq*100, class_iou*100))                              
                 # save model if performance is improved
                 if best_val_PQ<class_PQ:
                     best_val_PQ=class_PQ
                     torch.save(my_model.state_dict(), model_save_path)
-                print('Current val PQ is %.3f while the best val PQ is %.3f' %
+                log_writer.print_write_log('Current val PQ is %.3f while the best val PQ is %.3f' %
                     (class_PQ*100,best_val_PQ*100))               
-                print('Current val miou is %.3f'%
+                log_writer.print_write_log('Current val miou is %.3f'%
                     (miou*100))
 
                 if start_training:
                     sem_l ,hm_l, os_l = np.mean(loss_fn.lost_dict['semantic_loss']), np.mean(loss_fn.lost_dict['heatmap_loss']), np.mean(loss_fn.lost_dict['offset_loss'])
-                    print('epoch %d iter %5d, loss: %.3f, semantic loss: %.3f, heatmap loss: %.3f, offset loss: %.3f\n' %
+                    log_writer.print_write_log('epoch %d iter %5d, loss: %.3f, semantic loss: %.3f, heatmap loss: %.3f, offset loss: %.3f\n' %
                         (epoch, i_iter, sem_l+hm_l+os_l, sem_l, hm_l, os_l))
-                print('%d exceptions encountered during last training\n' %
+                log_writer.print_write_log('%d exceptions encountered during last training\n' %
                     exce_counter)
                 exce_counter = 0
                 loss_fn.reset_loss_dict()
 
             # training
             try:
-                train_vox_fea_ten = train_vox_fea.to(pytorch_device)
+                train_vox_fea_ten = train_vox_fea.to(pytorch_device, non_blocking=True)
                 train_label_tensor = SemKITTI2train(train_label_tensor)
-                train_pt_fea_ten = [torch.from_numpy(i).type(torch.FloatTensor).to(pytorch_device) for i in train_pt_fea]
-                train_grid_ten = [torch.from_numpy(i[:,:2]).to(pytorch_device) for i in train_grid]
-                train_label_tensor=train_label_tensor.type(torch.LongTensor).to(pytorch_device)
-                train_gt_center_tensor = train_gt_center.to(pytorch_device)
-                train_gt_offset_tensor = train_gt_offset.to(pytorch_device)
+                train_pt_fea_ten = [torch.from_numpy(i).type(torch.FloatTensor).to(pytorch_device, non_blocking=True) for i in train_pt_fea]
+                train_grid_ten = [torch.from_numpy(i[:,:2]).to(pytorch_device, non_blocking=True) for i in train_grid]
+                train_label_tensor=train_label_tensor.type(torch.LongTensor).to(pytorch_device, non_blocking=True)
+                train_gt_center_tensor = train_gt_center.to(pytorch_device, non_blocking=True)
+                train_gt_offset_tensor = train_gt_offset.to(pytorch_device, non_blocking=True)
 
                 if args['model']['enable_SAP'] and epoch>=args['model']['SAP']['start_epoch']:
                     for fea in train_pt_fea_ten:
@@ -207,7 +223,7 @@ def main(args):
                 optimizer.step()
             except Exception as error: 
                 if exce_counter == 0:
-                    print(error)
+                    log_writer.print_write_log(error)
                 exce_counter += 1
             
             # zero the parameter gradients
@@ -231,6 +247,6 @@ if __name__ == '__main__':
         new_args = yaml.safe_load(s)
     args = merge_configs(args,new_args)
 
-    print(' '.join(sys.argv))
-    print(args)
+    log_writer.print_write_log(' '.join(sys.argv))
+    log_writer.print_write_log(args)
     main(args)
